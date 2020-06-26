58c6ba18e02833c366245a1866e506bee3e1c2fd
Celina Jackson <celinanjackson@gmail.com>
2020-06-26 18:34:03 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,11 @@
 def unsafe?(speed)
-
+speed < 40 || speed > 60 ? true : false
 end
 
 
 
 def not_safe?(speed)
-	
+speed < 40 || speed > 60 ? true : false
 end
-	
-
-
+ not_safe?(35)
+ not_safe?(75)
